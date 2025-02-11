54c5d9fa69c19a2bee3fd90f614b51c642f46f23
Amir_Etemadi <amiretemadi@gmail.com>
2025-02-11 21:08:27 +0330

    Category model added

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural= 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author= models.ForeignKey(User, on_delete=models.CASCADE)
+    category= models.ManyToManyField(Category)
     created_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     image = models.ImageField(upload_to='images/posts')
